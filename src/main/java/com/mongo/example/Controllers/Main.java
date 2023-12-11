aa4e3f0b2f30f138afd2b610b196309f9f55fcf3
Administrator <sondagarjevin789@gmail.com>
2023-12-11 18:35:20 +0530

    complete bacend

--- src/main/java/com/mongo/example/Controllers/Main.java
+++ src/main/java/com/mongo/example/Controllers/Main.java
@@ -1,7 +1,56 @@
+// package com.mongo.example.Controllers;
+
+// import org.springframework.beans.factory.annotation.Autowired;
+// import org.springframework.http.ResponseEntity;
+// import org.springframework.web.bind.annotation.DeleteMapping;
+// import org.springframework.web.bind.annotation.GetMapping;
+// import org.springframework.web.bind.annotation.PathVariable;
+// import org.springframework.web.bind.annotation.PostMapping;
+// import org.springframework.web.bind.annotation.PutMapping;
+// import org.springframework.web.bind.annotation.RequestBody;
+// import org.springframework.web.bind.annotation.RequestMapping;
+// import org.springframework.web.bind.annotation.RestController;
+
+// import com.mongo.example.Dao.StudentRepo;
+// import com.mongo.example.Entities.Student;
+
+// @RestController
+// @RequestMapping("/student")
+// public class Main {
+//     @Autowired
+//     private StudentRepo studentRepo;
+
+//     @PostMapping("/")
+//     public ResponseEntity<?> addstudent(@RequestBody Student stud){
+//         Student save = this.studentRepo.save(stud);
+//         return ResponseEntity.ok(save);
+//     }
+
+//     @GetMapping("/")
+//     public ResponseEntity<?> getStudent(){
+        
+//         return ResponseEntity.ok(this.studentRepo.findAll());
+//     }
+
+//     @DeleteMapping("/{id}")
+//     public ResponseEntity<?> delete(@PathVariable("id") int id){
+//         this.studentRepo.deleteById(id);
+//         return ResponseEntity.ok().build();
+//     }
+
+//     @PutMapping("/{id}")
+//     public ResponseEntity<?> update(@PathVariable("id") int id, @RequestBody Student stud){
+//         stud.setId(id);
+//         this.studentRepo.save(stud);
+//         return ResponseEntity.ok().build();
+//     }
+// }
+
 package com.mongo.example.Controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -9,6 +58,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.mongo.example.Dao.StudentRepo;
@@ -16,7 +66,9 @@ import com.mongo.example.Entities.Student;
 
 @RestController
 @RequestMapping("/student")
+@CrossOrigin(origins = "http://localhost:3000") // Replace with the actual origin of your frontend
 public class Main {
+    
     @Autowired
     private StudentRepo studentRepo;
 
@@ -28,8 +80,12 @@ public class Main {
 
     @GetMapping("/")
     public ResponseEntity<?> getStudent(){
-        
         return ResponseEntity.ok(this.studentRepo.findAll());
+    }
+
+     @GetMapping("/{id}")
+    public ResponseEntity<?> getSingleStudent(@PathVariable("id") int id){
+        return ResponseEntity.ok(this.studentRepo.findById(id));
     }
 
     @DeleteMapping("/{id}")
